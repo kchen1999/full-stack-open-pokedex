392c0d5b3ad9ef75e3e1a46ffb0ae2ba11881b1f
Kevin Chen <kevin@Kevinui-MacBookPro.local>
2024-08-29 20:24:40 +1000

    add health check of Fly.io deployment

--- app.js
+++ app.js
@@ -13,3 +13,10 @@ app.listen(PORT, () => {
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+
+  res.send('ok')
+})
